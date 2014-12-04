3ee19fe8ee2ba7e4152e90ff1eeae753c82c0669
Orazz Atakishiyev <orazz.tm@gmail.com>
2014-12-04 22:20:49 +0500

    Initial commit

---
+++ SampleAlert/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleAlert
+//
+//  Created by Atakishiyev Orazdurdy on 12/4/14.
+//  Copyright (c) 2014 Atakishiyev Orazdurdy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
